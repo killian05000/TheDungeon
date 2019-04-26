2b3c5b3b672a05a38ed43a060bbd369a8b1e58bd
Killian Wolfger <killianwolfger@hotmail.fr>
2019-04-26 17:54:33 +0200

    adding player into the game class

--- core/src/main/java/com/mystudio/themaze/Game.java
+++ core/src/main/java/com/mystudio/themaze/Game.java
@@ -12,27 +12,31 @@ public class Game extends BasicGame {
 
 	private Texture texture;
 	private Maze maze;
+	private Player player;
 	
 	@Override
     public void initialise() {
     	texture = new Texture("mini2Dx.png");
     	maze = new Maze();
+    	player = new Player();
     }
     
     @Override
-    public void update(float delta) {
-    
+    public void update(float delta) 
+    {
+    	player.update();
     }
     
     @Override
     public void interpolate(float alpha) {
-    
+    	player.interpolate(1);
     }
     
     @Override
     public void render(Graphics g) 
 	{
 		g.drawTexture(texture, 0f, 0f);
+		player.render(g);
 		maze.PaintMaze(g);
     }
 	
