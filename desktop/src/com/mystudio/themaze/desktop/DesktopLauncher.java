2541a50db2a9b0320204ac3ece73714add923e51
Killian Wolfger <killianwolfger@hotmail.fr>
2019-04-26 15:34:33 +0200

    intial commit (project base)

---
+++ desktop/src/com/mystudio/themaze/desktop/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.mystudio.themaze.desktop;
+
+import org.mini2Dx.desktop.DesktopMini2DxConfig;
+
+import com.badlogic.gdx.backends.lwjgl.DesktopMini2DxGame;
+
+import com.mystudio.themaze.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		DesktopMini2DxConfig config = new DesktopMini2DxConfig(Game.GAME_IDENTIFIER);
+		config.vSyncEnabled = true;
+		new DesktopMini2DxGame(new Game(), config);
+	}
+}
