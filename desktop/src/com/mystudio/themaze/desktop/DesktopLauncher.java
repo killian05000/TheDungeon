532d7c7a6671825f402f1960efa98f9eb44ee6ab
Killian Wolfger <killianwolfger@hotmail.fr>
2019-04-26 15:43:17 +0200

    window modifications test

--- desktop/src/com/mystudio/themaze/desktop/DesktopLauncher.java
+++ desktop/src/com/mystudio/themaze/desktop/DesktopLauncher.java
@@ -7,9 +7,14 @@ import com.badlogic.gdx.backends.lwjgl.DesktopMini2DxGame;
 import com.mystudio.themaze.Game;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		DesktopMini2DxConfig config = new DesktopMini2DxConfig(Game.GAME_IDENTIFIER);
-		config.vSyncEnabled = true;
-		new DesktopMini2DxGame(new Game(), config);
+	
+	public static void main(String [] args) 
+	{
+		DesktopMini2DxConfig cfg = new DesktopMini2DxConfig(Game.GAME_IDENTIFIER);
+		cfg.title = "The Maze";
+		cfg.width = 800;
+		cfg.height = 600;
+		cfg.vSyncEnabled = true;
+		new DesktopMini2DxGame(new Game(), cfg);
 	}
 }
