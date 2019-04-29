998db68c4853797baad869892edeb4ad548136cb
Killian Wolfger <killianwolfger@hotmail.fr>
2019-04-29 13:42:33 +0200

    direction reinstated

--- core/src/main/java/com/mystudio/themaze/Player.java
+++ core/src/main/java/com/mystudio/themaze/Player.java
@@ -14,6 +14,8 @@ public class Player
 	private int defaultPostionX = 1;
 	private int defaultPositionY = 9;
 	
+	private int direction;
+	
 	private Texture badpac;
 	
 	public Player() 
@@ -33,52 +35,53 @@ public class Player
 		if(Gdx.input.isKeyPressed(Keys.UP)) 
 		{
 			badpac = new Texture("badpacUp.png");
-			newPosX= getPosX()-1;
-			if(newPosX>=0 && maze.getMatrix()[newPosX][getPosY()] == 0)
-				setPosX(newPosX);
+			direction=0;
 		}
     	if(Gdx.input.isKeyPressed(Keys.RIGHT))
     	{
     		badpac = new Texture("badpacRight.png");
-    		newPosY = getPosY()+1;
-			if(newPosY<maze.getMatrix()[0].length && maze.getMatrix()[getPosX()][newPosY] == 0)
-				setPosY(newPosY);
+    		direction=1;
     	}
     	if(Gdx.input.isKeyPressed(Keys.DOWN))
     	{
     		badpac = new Texture("badpacDown.png");
-    		newPosX = getPosX()+1;
-			if(newPosX<maze.getMatrix().length && maze.getMatrix()[newPosX][getPosY()] == 0) 
-				setPosX(newPosX);	
+    		direction=2;
     	}
     	if(Gdx.input.isKeyPressed(Keys.LEFT))
     	{
-    		badpac = new Texture("badpacLeft.png");  
-    		newPosY = getPosY()-1;
-			if(newPosY>=0 && maze.getMatrix()[getPosX()][newPosY] == 0) 
-				setPosY(newPosY);
+    		badpac = new Texture("badpacLeft.png");
+    		direction=3;
     	}
+    	
+    	switch(direction)
+    	{
+			case 0:
+				newPosX= posX-1;
+				if(newPosX>=0 && maze.getMatrix()[newPosX][posY] == 0)
+					posX=newPosX;
+				break;
+			case 1:
+				newPosY = posY+1;
+				if(newPosY<maze.getMatrix()[0].length && maze.getMatrix()[posX][newPosY] == 0)
+					posY=newPosY;
+				break;
+			case 2:
+				newPosX = posX+1;
+				if(newPosX<maze.getMatrix().length && maze.getMatrix()[newPosX][posY] == 0) 
+					posX=newPosX;			
+				break;
+			case 3:
+				newPosY = posY-1;
+				if(newPosY>=0 && maze.getMatrix()[posX][newPosY] == 0) 
+					posY=newPosY;
+				break;	
+		}
 	}
 	
 	
 	public void render(Graphics g, int mapScale) 
 	{	
 		g.drawTexture(badpac, posY*mapScale, posX*mapScale, mapScale, mapScale);
-	}
-	
-	public int getPosX() 
-	{
-		return posX;
-	}
-	
-	public int getPosY() 
-	{
-		return posY;
-	}
-	
-	public void setPosX(int posX) 
-	{
-		this.posX = posX;
 	}
 	
 	public void setPosY(int posY) 
