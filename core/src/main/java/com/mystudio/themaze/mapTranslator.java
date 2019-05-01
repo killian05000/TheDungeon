4b696ad7c3acb4aa8bc0110af1ed002c768b3b42
Killian Wolfger <killianwolfger@hotmail.fr>
2019-05-01 16:22:29 +0200

    bug fixed, now work much better, + refactoring

--- core/src/main/java/com/mystudio/themaze/mapTranslator.java
+++ core/src/main/java/com/mystudio/themaze/mapTranslator.java
@@ -9,81 +9,107 @@ import javax.imageio.ImageIO;
 public class mapTranslator 
 {
 	private BufferedImage map;
-	private int tileSize;
-	private int[][] M;
+	private int tileSize = 32;
+	private int[][] matrix;
 	
-	public mapTranslator(String path) throws IOException
+	/**
+	 * @param path : skeleton image file path
+	 */
+	public mapTranslator(String path)
 	{
-		System.out.println(System.getProperties().get("user.dir"));
-		map = ImageIO.read(new File("../assets/"+path));
-		tileSize = 30;
-		M = new int[map.getWidth()/tileSize][map.getHeight()/tileSize];
-		System.out.println(map.getWidth()/tileSize);
-		System.out.println(map.getHeight());
-		System.out.println(map.getWidth());
+		try
+		{
+			map = ImageIO.read(new File("../assets/"+path));
+		}
+		catch(IOException e)
+		{
+			System.err.println("Error during the skeleton map translation");
+			e.printStackTrace();
+		}
+		
+		matrix = new int[map.getHeight()/tileSize][map.getWidth()/tileSize];		
 	}
 	
+	/**
+	 * Go through the image skeleton and get each of its tile's RGB value
+	 */
 	public void translate()
 	{
-		int xM = 0;
-		int yM = 0;		
+		int xM = 0; // Matrix x
+		int yM = 0;	// Matrix y	
 
-		for(int x=tileSize/2; x<map.getHeight(); x+=tileSize)
+		for(int x = tileSize/2; x < map.getHeight(); x += tileSize)
 		{
-			for(int y=tileSize/2; y<map.getWidth(); y+=tileSize)
+			for(int y = tileSize/2; y < map.getWidth(); y += tileSize)
 			{
-				int pixel=map.getRGB(x,y);
-				int[] v = new int[3];
+				int pixel = map.getRGB(y,x); // get the RGB value of a pixel
+				
+				// Split the pixel R, G and B value in a vector
+				int[] v = new int[3]; 
 				v[0] = (pixel >> 16) & 0xff;
 				v[1] = (pixel >> 8) & 0xff;
 				v[2] = pixel & 0xff;
 				
-				//System.out.println("["+x+","+y+"] : "+"("+v[0]+","+v[1]+","+v[2]+")");				
+				//System.out.println("["+x+","+y+"] : "+"("+v[0]+","+v[1]+","+v[2]+")");
 				buildMatrix(v,xM,yM);
-				xM++;
+				yM++;
 			}
-			xM=0;
-			yM++;
+			yM=0;
+			xM++;
 		}
-		//displayMatrix();		
+		displayMatrix();		
 	}
 	
+	/**
+	 * Build the core matrix of the game according to the image skeleton color code
+	 * @param p : RGB vector
+	 * @param x : axis x of the matrix
+	 * @param y : axis y of the matrix
+	 */
 	public void buildMatrix(int[] p, int x, int y) // (x = line,y = column)
 	{
-		if(p[0]==198 && p[1]==198 && p[2]==198) //ALLEYS
-			M[x][y] = 0;
-		else if(p[0]==60 && p[1]==60 && p[2]==60) //WALLS
-			M[x][y] = 1;
-		else if(p[0]==103 && p[1]==175 && p[2]==233) //PLAYER SPAWN
-			M[x][y] = 2;
-		else if(p[0]==174 && p[1]==64 && p[2]==64) //ENEMY SPAWN
-			M[x][y] = 3;
+		if(p[0]==198 && p[1]==198 && p[2]==198) // ALLEYS
+			matrix[x][y] = 0;
+		else if(p[0]==60 && p[1]==60 && p[2]==60) // WALLS
+			matrix[x][y] = 1;
+		else if(p[0]==103 && p[1]==175 && p[2]==233) // PLAYER SPAWN
+			matrix[x][y] = 2;
+		else if(p[0]==174 && p[1]==64 && p[2]==64) // ENEMY SPAWN
+			matrix[x][y] = 3;
 		else if(p[0]==111 && p[1]==19 && p[2]==17) // TRAPS
-			M[x][y] = 4;
+			matrix[x][y] = 4;
 		else if(p[0]==95 && p[1]==76 && p[2]==54) // EXIT DOOR
-			M[x][y] = 5;
+			matrix[x][y] = 5;
 		else if(p[0]==158 && p[1]==233 && p[2]==135) // EXIT
-			M[x][y] = 6;		
-		else if(p[0]==163 && p[1]==104 && p[2]==162) //TELEPORTER
-			M[x][y] = 9;		
+			matrix[x][y] = 6;		
+		else if(p[0]==163 && p[1]==104 && p[2]==162) // TELEPORTER
+			matrix[x][y] = 9;		
 	}
 	
+	/**
+	 * Display the game matrix
+	 */
 	public void displayMatrix()
 	{
-		for(int x=0; x<M.length; x+=1)
+		for(int x=0; x<matrix.length; x+=1)
 		{
-			for(int y=0; y<M[0].length; y+=1)
+			for(int y=0; y<matrix[0].length; y+=1)
 			{
-				System.out.print(M[x][y]+",");
+				System.out.print(matrix[x][y]+",");
 			}
 			System.out.println();
 		}
-		System.out.println("Width : "+M.length);
-		System.out.println("Height : "+M[0].length);
+		System.out.println("Width : "+matrix.length);
+		System.out.println("Height : "+matrix[0].length);
 	}
 	
 	public int[][] getMatrix()
 	{
-		return M;
+		return matrix;
+	}
+	
+	public int getTileSize()
+	{
+		return tileSize;
 	}
 }
