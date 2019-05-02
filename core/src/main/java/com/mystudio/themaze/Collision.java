7c72d7cc0908292fbccc644155f579d5bf9eb632
Killian Wolfger <killianwolfger@hotmail.fr>
2019-05-02 14:07:51 +0200

    player now collide with enemies

--- core/src/main/java/com/mystudio/themaze/Collision.java
+++ core/src/main/java/com/mystudio/themaze/Collision.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 public class Collision {
 	Player player;
 	ArrayList<Item> items;
+	ArrayList<Enemy> enemies;
 	int mapScale;
 	
 	/**
@@ -13,10 +14,11 @@ public class Collision {
 	 * @param _player : player instance
 	 * @param scale : tile size
 	 */
-	public Collision(ArrayList<Item> _items, Player _player, int scale) 
+	public Collision(ArrayList<Item> _items, Player _player, ArrayList<Enemy> _enemies, int scale) 
 	{
 		player = _player;
 		items = _items;
+		enemies = _enemies;
 		mapScale = scale;
 	}
 	
@@ -35,7 +37,7 @@ public class Collision {
 			{
 				player.addItem(items.get(i));
 				System.out.println(player.getBag().size());
-				items.remove(i);
+				//items.remove(i);
 				break;
 			}
 			
@@ -47,7 +49,35 @@ public class Collision {
 				{
 					player.addItem(items.get(i));
 					System.out.println(player.getBag().size());
-					items.remove(i);
+					//items.remove(i);
+					break;
+				}		
+		}
+	}
+	
+	public void verifyEnemy() 
+	{
+		for(int i=0; i<enemies.size(); i++) 
+		{
+			if(player.getPosX() + mapScale - player.getSpeed() >= enemies.get(i).getPosX() 
+				&& player.getPosY() >= enemies.get(i).getPosY()
+				&& player.getPosX() <= enemies.get(i).getPosX() + mapScale - player.getSpeed()
+				&& player.getPosY() <= enemies.get(i).getPosY() + mapScale - player.getSpeed()
+				) 
+			{
+				System.out.println("CRUNCH");
+				player.setAlive(false);
+				break;
+			}
+			
+			if(player.getPosY() + mapScale - player.getSpeed() >= enemies.get(i).getPosY() 
+					&& player.getPosX() + mapScale - player.getSpeed() >= enemies.get(i).getPosX()
+					&& player.getPosY() + mapScale - player.getSpeed() <= enemies.get(i).getPosY() + mapScale - player.getSpeed()
+					&& player.getPosX() <= enemies.get(i).getPosX() + mapScale - player.getSpeed()
+					) 
+				{
+					System.out.println("CRUNCH");
+					player.setAlive(false);
 					break;
 				}		
 		}
