da2757ed331598a8a0adf49bab31afeb6fba020b
Killian Wolfger <killianwolfger@hotmail.fr>
2019-05-03 16:27:05 +0200

    objects-enemies collision added + called functions optimization

--- core/src/main/java/com/mystudio/themaze/Collision.java
+++ core/src/main/java/com/mystudio/themaze/Collision.java
@@ -27,12 +27,21 @@ public class Collision {
 	 */
 	public void verify() 
 	{
+		int pPosX, pPosY, iPosX, iPosY, pSpeed = 0;
+		pPosX = player.getPosX();
+		pPosY = player.getPosY();
+		pSpeed = player.getSpeed();
+		
 		for(int i=0; i<items.size(); i++) 
-		{
-			if(player.getPosX() + mapScale - player.getSpeed() >= items.get(i).getPosX() 
-				&& player.getPosY() >= items.get(i).getPosY()
-				&& player.getPosX() <= items.get(i).getPosX() + mapScale - player.getSpeed()
-				&& player.getPosY() <= items.get(i).getPosY() + mapScale - player.getSpeed()
+		{		
+			iPosX = items.get(i).getPosX();
+			iPosY = items.get(i).getPosY();
+			
+			
+			if(pPosX + mapScale - pSpeed >= iPosX 
+				&& pPosY >= iPosY
+				&& pPosX <= iPosX + mapScale - pSpeed
+				&& pPosY <= iPosY + mapScale - pSpeed
 				) 
 			{
 				player.addItem(items.get(i));
@@ -41,45 +50,78 @@ public class Collision {
 				break;
 			}
 			
-			if(player.getPosY() + mapScale - player.getSpeed() >= items.get(i).getPosY() 
-					&& player.getPosX() + mapScale - player.getSpeed() >= items.get(i).getPosX()
-					&& player.getPosY() + mapScale - player.getSpeed() <= items.get(i).getPosY() + mapScale - player.getSpeed()
-					&& player.getPosX() <= items.get(i).getPosX() + mapScale - player.getSpeed()
+			if(pPosY + mapScale - pSpeed >= iPosY 
+					&& pPosX + mapScale - pSpeed >= iPosX
+					&& pPosY + mapScale - pSpeed <= iPosY + mapScale - pSpeed
+					&& pPosX <= iPosX + mapScale - pSpeed
 					) 
-				{
-					player.addItem(items.get(i));
-					System.out.println(player.getBag().size());
-					//items.remove(i);
-					break;
-				}		
+			{
+				player.addItem(items.get(i));
+				System.out.println(player.getBag().size());
+				//items.remove(i);
+				break;
+			}		
 		}
 	}
 	
 	public void verifyEnemy() 
 	{
+		int pPosX, pPosY, pSpeed, ePosX, ePosY, eSpeed, iPosX, iPosY = 0;
+		pPosX = player.getPosX();
+		pPosY = player.getPosY();
+		pSpeed = player.getSpeed();
+		
 		for(int i=0; i<enemies.size(); i++) 
 		{
-			if(player.getPosX() + mapScale - player.getSpeed() >= enemies.get(i).getPosX() 
-				&& player.getPosY() >= enemies.get(i).getPosY()
-				&& player.getPosX() <= enemies.get(i).getPosX() + mapScale - player.getSpeed()
-				&& player.getPosY() <= enemies.get(i).getPosY() + mapScale - player.getSpeed()
+			ePosX = enemies.get(i).getPosX();
+			ePosY = enemies.get(i).getPosY();
+			eSpeed = enemies.get(i).getSpeed();
+			
+			if(pPosX + mapScale - pSpeed >= ePosX 
+				&& pPosY >= ePosY
+				&& pPosX <= ePosX + mapScale - pSpeed
+				&& pPosY <= ePosY + mapScale - pSpeed
 				) 
 			{
-				System.out.println("CRUNCH");
 				player.setAlive(false);
 				break;
 			}
 			
-			if(player.getPosY() + mapScale - player.getSpeed() >= enemies.get(i).getPosY() 
-					&& player.getPosX() + mapScale - player.getSpeed() >= enemies.get(i).getPosX()
-					&& player.getPosY() + mapScale - player.getSpeed() <= enemies.get(i).getPosY() + mapScale - player.getSpeed()
-					&& player.getPosX() <= enemies.get(i).getPosX() + mapScale - player.getSpeed()
+			if(pPosY + mapScale - pSpeed >= ePosY 
+					&& pPosX + mapScale - pSpeed >= ePosX
+					&& pPosY + mapScale - pSpeed <= ePosY + mapScale - pSpeed
+					&& pPosX <= ePosX + mapScale - pSpeed
 					) 
-				{
-					System.out.println("CRUNCH");
+			{
 					player.setAlive(false);
 					break;
-				}		
+			}		
+			
+			for(int j=0; j<items.size(); j++)
+			{
+				iPosX = items.get(j).getPosX();
+				iPosY = items.get(j).getPosY();
+				
+				if(ePosX + mapScale - eSpeed >= iPosX 
+						&& ePosY >= iPosY
+						&& ePosX <= iPosX + mapScale - eSpeed
+						&& ePosY <= iPosY + mapScale - eSpeed
+						) 
+					{
+						enemies.get(i).setAlive(false);
+						break;
+					}
+					
+					if(ePosY + mapScale - eSpeed >= iPosY 
+							&& ePosX + mapScale - eSpeed >= iPosX
+							&& ePosY + mapScale - eSpeed <= iPosY + mapScale - eSpeed
+							&& ePosX <= iPosX + mapScale - eSpeed
+							) 
+					{
+						enemies.get(i).setAlive(false);
+						break;
+					}
+			}
 		}
 	}
 }
