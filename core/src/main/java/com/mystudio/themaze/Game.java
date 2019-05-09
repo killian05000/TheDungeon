a6446f427bc0af6f852cbcaf59e1a41b5c75a357
Killian Wolfger <killianwolfger@hotmail.fr>
2019-05-09 11:14:35 +0200

    uncomment

--- core/src/main/java/com/mystudio/themaze/Game.java
+++ core/src/main/java/com/mystudio/themaze/Game.java
@@ -30,9 +30,9 @@ public class Game extends BasicGame {
     	
     	player = new Player(maze.getPlayerSpawnX(), maze.getPlayerSpawnY(), maze.getMapScale(), maze, eventListener);
     	enemies = new ArrayList<Enemy>();
-    	//enemies.add(new Enemy(maze.getEnemySpawnX(0), maze.getEnemySpawnY(0), maze.getMapScale(), maze));
-    	//enemies.add(new Enemy(maze.getEnemySpawnX(1), maze.getEnemySpawnY(1), maze.getMapScale(), maze));
-    	//enemies.add(new Enemy(maze.getEnemySpawnX(2), maze.getEnemySpawnY(2), maze.getMapScale(), maze));
+    	enemies.add(new Enemy(maze.getEnemySpawnX(0), maze.getEnemySpawnY(0), maze.getMapScale(), maze));
+    	enemies.add(new Enemy(maze.getEnemySpawnX(1), maze.getEnemySpawnY(1), maze.getMapScale(), maze));
+    	enemies.add(new Enemy(maze.getEnemySpawnX(2), maze.getEnemySpawnY(2), maze.getMapScale(), maze));
     	
 //    	maze.addItem(new Item(23, 27, maze.getMapScale(), "item/key.png", maze));
 //    	maze.addItem(new Item(23, 28, maze.getMapScale(), "item/sword.png", maze));
@@ -87,7 +87,6 @@ public class Game extends BasicGame {
     {
     	if(Gdx.input.isKeyJustPressed(Keys.R)) 
 		{
-			player.setAlive(true);
 			for(int i=0; i<enemies.size(); i++)
 				enemies.get(i).respawn();
 			for(int j=0; j<maze.getItems().size(); j++)
