bdfba1acf2e6b8694e52d3bfbe3fdabbb5e010d9
Killian Wolfger <killianwolfger@hotmail.fr>
2019-05-09 11:13:46 +0200

    new sounds, new way to handle them, but can be improved

--- core/src/main/java/com/mystudio/themaze/MusicHandler.java
+++ core/src/main/java/com/mystudio/themaze/MusicHandler.java
@@ -3,35 +3,92 @@ package com.mystudio.themaze;
 import static javax.sound.sampled.Clip.LOOP_CONTINUOUSLY;
 
 import java.io.File;
+import java.util.ArrayList;
 
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 
-public class Song {
-	String path;
+public class MusicHandler {
+	ArrayList<Clip> sounds;
 	
-	public Song(String path) {
-		this.path = path;
-	}
-	
-	public void startWithoutLoop() {
-		try {
-			Clip clip = AudioSystem.getClip();
-			clip.open(AudioSystem.getAudioInputStream(new File("../assets/music/"+path)));
-			clip.start();
-		} catch (Exception e) {
-			e.getStackTrace();
-		} 
-	}
-	
-	public void startLoop() {
-		try {
-			Clip clip = AudioSystem.getClip();
-			clip.open(AudioSystem.getAudioInputStream(new File("../assets/music/"+path)));
+	public MusicHandler() {
+		try 
+		{
+			System.out.println("1");
+
+			Clip backgroundMusic = AudioSystem.getClip();
+			backgroundMusic.open(AudioSystem.getAudioInputStream(new File("../assets/music/Pim Poy.wav")));
 			
-			clip.loop(LOOP_CONTINUOUSLY);
-		} catch (Exception e) {
+			System.out.println("2");
+
+			
+			Clip gameOver = AudioSystem.getClip();
+			gameOver.open(AudioSystem.getAudioInputStream(new File("../assets/music/Game Over.wav")));
+			
+			System.out.println("3");
+
+			
+			Clip win = AudioSystem.getClip();
+			win.open(AudioSystem.getAudioInputStream(new File("../assets/music/Win.wav")));
+			
+			System.out.println("4");
+			
+			Clip getItem = AudioSystem.getClip();
+			getItem.open(AudioSystem.getAudioInputStream(new File("../assets/music/getItem.wav")));
+			
+			System.out.println("5");
+			
+			Clip throwItem = AudioSystem.getClip();
+			throwItem.open(AudioSystem.getAudioInputStream(new File("../assets/music/throwItem.wav")));
+			
+			System.out.println("6");			
+			
+			sounds = new ArrayList<Clip>();
+			sounds.add(backgroundMusic);
+			sounds.add(gameOver);
+			sounds.add(win);
+			sounds.add(getItem);
+			sounds.add(throwItem);
+			System.out.println("sounds DONE");
+		} catch (Exception e) 
+		{
 			e.getStackTrace();
 		} 
-	}	
+	}
+	
+	public void startGameMusic()
+	{
+		sounds.get(1).stop();
+		sounds.get(2).stop();
+		sounds.get(0).loop(LOOP_CONTINUOUSLY);
+	}
+	
+	public void stopGameMusic()
+	{
+		sounds.get(0).stop();
+	}
+	
+	public void gameOver()
+	{
+		sounds.get(0).stop();
+		sounds.get(1).start();
+	}
+	
+	public void gameWin()
+	{
+		sounds.get(0).stop();
+		sounds.get(2).start();
+	}
+	
+	public void getItem()
+	{
+		sounds.get(3).setFramePosition(0);
+		sounds.get(3).start();		
+	}
+	
+	public void throwItem()
+	{
+		sounds.get(4).setFramePosition(0);
+		sounds.get(4).start();
+	}
 }
