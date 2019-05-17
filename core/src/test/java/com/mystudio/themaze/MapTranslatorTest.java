fce21b04b74a84e992b5e035d4bd849ad57dfb72
Allaoui5 <abderemane.allaoui@etu.univ-amu.fr>
2019-05-17 14:53:55 +0200

    next Gradle

---
+++ core/src/test/java/com/mystudio/themaze/MapTranslatorTest.java
@@ -0,0 +1,17 @@
+package com.mystudio.themaze;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MapTranslatorTest {
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+		MapTranslator map = new MapTranslator("/map/mapSkeleton.png");
+		
+		
+	}
+
+}
