b462d6ef439fa3eeb5253a3a2c1c4bca6042b100
IssamAmu <issam.bouras@etu.univ-amu.fr>
2019-05-22 12:49:41 +0200

    Window renaming

--- desktop/src/com/mystudio/themaze/desktop/DesktopLauncher.java
+++ desktop/src/com/mystudio/themaze/desktop/DesktopLauncher.java
@@ -4,14 +4,14 @@ import org.mini2Dx.desktop.DesktopMini2DxConfig;
 
 import com.badlogic.gdx.backends.lwjgl.DesktopMini2DxGame;
 
-import com.mystudio.themaze.Game;
+import game.Game;
 
 public class DesktopLauncher {
 	
 	public static void main(String [] args) 
 	{
 		DesktopMini2DxConfig cfg = new DesktopMini2DxConfig(Game.GAME_IDENTIFIER);
-		cfg.title = "The Maze";
+		cfg.title = "The Dungeon";
 		cfg.width = 1024;
 		cfg.height = 768;
 		cfg.vSyncEnabled = true;
